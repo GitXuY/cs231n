483fd2865250396ec3fbdeb1a05ab7b9fb2cb71d
Dijkstraaaaa <xuy@bupt.edu.cn>
2016-12-08 10:51:47 -0800

    12-8 momentum

--- assignment2/cs231n/optim.py
+++ assignment2/cs231n/optim.py
@@ -65,11 +65,16 @@ def sgd_momentum(w, dw, config=None):
   # TODO: Implement the momentum update formula. Store the updated value in   #
   # the next_w variable. You should also use and update the velocity v.       #
   #############################################################################
-  pass
+  m = config['momentum']
+  lr = config['learning_rate']
+  # Momentum update
+  v = m * v - lr * dw # integrate velocity
+  w += v # integrate position
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
   config['velocity'] = v
+  next_w = w
 
   return next_w, config
 
